322d9075d2918cf22863efc8dad503f2b613c97e
Azim Hatami <azimhatami4@gmail.com>
2024-04-11 14:23:27 +0330

    Add more validations

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get('/items/')
-async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+async def read_items(
+    q: Annotated[str | None,  Query(min_length=3, hmax_length=50)] = None
+):
     results = {'items': [{'item_id': 'Foo'}, {'item_id': 'Bar'}]}
     if q:
         results.update({'q': q})
