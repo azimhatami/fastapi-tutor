bd7fd55f4c190e0d85ef6a63621f911536fd35d0
Azim Hatami <azimhatami4@gmail.com>
2024-04-11 09:25:57 +0330

    Learn request body

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 app = FastAPI()
 
-fake_items_db = [
-    {'item_name': 'Foo'},
-    {'item_name': 'Bar'},
-    {'item_name': 'Baz'},
-]
-
-@app.get('/items/{item_id}')
-async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: int | None = None
-):
-    item = {'item_id': item_id, 'needy': needy, 'skip': skip, 'limit': limit}
-    return item
+@app.put('/items/{item_id}')
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {'item_id': item_id, **item.dict()}
+    if q:
+        result.update({'q': q})
+    return result
