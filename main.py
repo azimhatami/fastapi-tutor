1917fd30885216e8a4a0f48c24aa2b9d6fdd0057
Azim Hatami <azimhatami4@gmail.com>
2024-04-11 16:49:25 +0330

    Declare more metadata

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[list | None, Query()] = []):
-    query_items = {"q": q}
-    return query_items
+async def read_items(
+    q: Annotated[
+        str | None,
+        Query(
+            title='Query string',
+            min_length=3,
+            description = 'Query string for the items to search in the database that have a good match'
+        )
+    ] = None,
+):
+    results = {'items': [{'item_id': 'Foo'}, {'item_id': 'Bar'}]}
+    if q:
+        results.update({'q': q})
+    return results
