7c7710407e1d4fa1c53ee0b403378583fb970048
Azim Hatami <azimhatami4@gmail.com>
2024-04-11 16:23:59 +0330

    Query parameter list

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI, Query
 from typing import Annotated
 
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
-@app.get('/items/')
-async def read_items(q: Annotated[str | None, Query(min_length=3)] = ...):
-    results = {'items': [{'item_id': 'Foo'}, {'item_id': 'Bar'}]}
-    if q:
-        results.update({'q': q})
-    return results
+
+@app.get("/items/")
+async def read_items(q: Annotated[list | None, Query()] = []):
+    query_items = {"q": q}
+    return query_items
