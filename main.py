0918b5673f49d6c98dab6ff1f8a29effe06ed24a
Azim Hatami <azimhatami4@gmail.com>
2024-04-12 09:35:12 +0330

    Exclude from OpenAPI

--- main.py
+++ main.py
@@ -7,20 +7,9 @@ app = FastAPI()
 
 @app.get("/items/")
 async def read_items(
-    q: Annotated[
-        str | None,
-        Query(
-            alias="item-query",
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-            max_length=50,
-            pattern="^fixedquery$",
-            deprecated=True,
-        ),
-    ] = None,
+    hidden_query: Annotated[str | None, Query(include_in_schema=False)] = None,
 ):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+    if hidden_query:
+        return {'hidden_query': hidden_query}
+    else:
+        return {'hidden_query': 'Not found'}
