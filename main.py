fbb1017f9dc45ca012a79333cae08b547f4f0383
Azim Hatami <azimhatami4@gmail.com>
2024-04-12 12:53:07 +0330

    Number validations: floats, greater than and less than

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def read_items(
     item_id: Annotated[int, Path(title='The ID of the item to get', gt=0, le=1000)],
-    q: str
+    q: str,
+    size: Annotated[float, Query(gt=0, lt=10.5)],
 ):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
+    if size:
+        results.update({'size': size})
     return results
