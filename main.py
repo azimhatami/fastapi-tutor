20fad34a6bb125fc6a26091aead6ec51cc05283c
Azim Hatami <azimhatami4@gmail.com>
2024-04-13 17:08:36 +0330

    Body - Fields

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Path, Query, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None, title='The description of the time', max_length=300
+    )
+    price: float = Field(
+        gt=0, description='The price must be greater than zero'
+    )
     tax: float | None = None
 
 
