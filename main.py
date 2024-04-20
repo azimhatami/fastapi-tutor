2da3cdc63fe1af7acf36baa3ebf438a2c9d2d86b
Azim Hatami <azimhatami4@gmail.com>
2024-04-20 16:12:03 +0330

    Invalid return type annotations

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Response
 from fastapi.responses import RedirectResponse
 
-
 app = FastAPI()
 
 
-@app.get('/teleport')
-async def get_teleport() -> RedirectResponse:
-    return RedirectResponse(url='https://w3schools.com/')
+@app.get("/portal")
+async def get_portal(teleport: bool = False) -> Response | dict:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return {"message": "Here's your interdimensional portal."}
