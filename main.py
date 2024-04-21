aea53343251dfa929dd8e4c031affc0d907063e8
Azim Hatami <azimhatami4@gmail.com>
2024-04-21 15:26:09 +0330

    Using list(s) instead of set(s)

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ items = {
 @app.get(
     '/items/{item_id}/name',
     response_model = Item,
-    response_model_include = {'name', 'description'},
+    response_model_include = ['name', 'description'],
 )
 async def read_item_name(item_id: str):
     return items[item_id]
@@ -43,7 +43,7 @@ async def read_item_name(item_id: str):
 @app.get(
     '/items/{item_id}/public',
     response_model = Item,
-    response_model_exclude = {'tax'}
+    response_model_exclude = ['tax']
 )
 async def read_item_public_data(item_id: str):
     return items[item_id]
