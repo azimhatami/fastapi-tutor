6c453c5b77bddc07caba1f6e1a1438d1d63a063f
Azim Hatami <azimhatami4@gmail.com>
2024-04-21 12:33:46 +0330

    Response model encoding parameters

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-from fastapi import FastAPI, Response
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/portal', response_model = None)
-async def get_portal(teleport: bool = False) -> Response | dict:
-    if teleport:
-        return RedirectResponse(url='https://w3schools.com/')
-    return {'message': 'Here\'s your interdimensional portal.'}
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float = 10.5
+    tags: list[str] = []
+
+
+items = {
+    'foo': {
+        'name': 'Foo',
+        'price': 50.2
+    },
+    'bar': {
+        'name': 'Bar',
+        'description': 'The bartenders',
+        'price': 62, 'tax': 20.2
+    },
+    'baz': {
+        'name': 'Baz',
+        'description': None,
+        'price': 50.2,
+        'tax': 10.5,
+        'tags': []
+    },
+}
+
+
+@app.get(
+    '/items/{item_id}',
+    response_model = Item,
+    response_model_exclude_unset = True
+)
+async def read_item(item_id: str):
+    return items[item_id]
